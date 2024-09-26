41a26e7ac61d2bbb0f8ad2268ee9acb32f2b11a5
Ali Umar <ali.umar.work@gmail.com>
2024-09-25 20:33:07 -0400

    read file and go thru tokens

--- a1/include/token.h
+++ a1/include/token.h
@@ -2,12 +2,31 @@
 #define TOKEN_H
 
 typedef enum {
-     TOKEN_EOF=0,
-     TOKEN_WHILE,
+     TOKEN_EOF=0, // enum index start 0
      TOKEN_ADD,
-     TOKEN_IDENT,
-     TOKEN_NUMBER,
+     TOKEN_SUBTRACT,
+     TOKEN_ARRAY,
+     TOKEN_BOOLEAN,
+     TOKEN_CHAR,
+     TOKEN_ELSE,
+     TOKEN_FALSE,
+     TOKEN_FOR,
+     TOKEN_FUNCTION,
+     TOKEN_IF,
+     TOKEN_INTEGER,
+     TOKEN_PRINT,
+     TOKEN_RETURN,
+     TOKEN_STRING,
+     TOKEN_TRUE,
+     TOKEN_VOID,
+     TOKEN_WHILE,
+     TOKEN_INTEGER_LITERAL,
+     TOKEN_CHARACTER_LITERAL,
+     TOKEN_IDENTIFIER,
      TOKEN_ERROR
 } token_t;
+
+// defined in constants.c, 1-1 mapping with token_t
+extern const char* TOKEN_LOOKUP[];
 
 #endif
